c0e859cd127b0fe6c443ba03891141e639eeadc2
Mattis Megevand <megevand.mattis@gmail.com>
2024-12-01 13:32:16 +0100

    aoc 24: day 1

---
+++ advent_of_code/24/1.py
@@ -0,0 +1,13 @@
+a, b = [], []
+for l in open('1.in', 'r').read().splitlines():
+    aa, bb = [int(t) for t in l.split()]
+    a.append(aa)
+    b.append(bb)
+a.sort()
+b.sort()
+
+part1 = sum(abs(aa - bb) for aa, bb in zip(a, b))
+part2 = sum(aa * b.count(aa) for aa in a)
+
+print(part1)
+print(part2)
